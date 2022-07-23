4b29e5dea3134d9bcbd04857e617dab09df8a791
Deng Lee <edaero22@naver.com>
2022-07-23 22:56:20 +0900

    add tailwindCSS

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Layout from '../components/Layout'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
+    <Layout>
+      {/* 타이틀 */}
+      <Head> 
         <title>댕 포트폴리오</title>
         <meta name="description" content="댕 포트폴리오 만들기" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <h1>화면</h1>
-    </div>
+      <h1 className="text-3xl font-bold underline">
+      홈 입니다.
+      </h1>
+    </Layout>
   )
 }
